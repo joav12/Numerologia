60dc6e863dd88335557adcd3fa8e9e0e78fddf5d
João Victor Rosinhole <61693806+joav12@users.noreply.github.com>
2023-08-03 15:09:21 -0300

    Add files via upload

--- js/formula.js
+++ js/formula.js
@@ -1,10 +1,38 @@
-var tem_user_nome = false;
+var n = 0;
 
 function nome(){
-    const nome = document.getElementById("name_user").value;
-    var tem_user_nome = true;
+    if(document.getElementById("name_user").value == ""){
+        window.alert("Preencha com o seu nome");
+    }
 
-    document.getElementById("name_user").setAttribute('style', 'display: none');
+    else{
+        n += 1;
+        const name_user = document.getElementById("name_user").value;
 
-    return Array(tem_user_nome, nome);
+        $('#name_user').css('display', 'none');
+
+        $('#formulario .input p').text("Informe sua data de nascimento");
+
+        $('.input-dropdown').css('display', 'flex');
+
+        if($('.input-dia').attr('value') == "Dia" && n >= 2 || $('.input-mes').attr('value') == "Mês" && n >= 2 || $('.input-ano').attr('value') == "Ano" && n >= 2){
+            window.alert("Por favor informe a sua data de aniversário para continuar");
+        }
+        else if($('.input-dia').attr('value') != "Dia" && n >= 2 || $('.input-mes').attr('value') != "Mês" && n >= 2 || $('.input-ano').attr('value') != "Ano" && n >= 2){
+            // window.alert("a");
+        }
+        
+    }
+}
+
+function add_dia(butao){
+    $('.input-dia').attr('value', $(butao).text());
+}
+
+function add_mes(butao){
+    $('.input-mes').attr('value', $(butao).text());
+}
+
+function add_ano(butao){
+    $('.input-ano').attr('value', $(butao).text());
 }
